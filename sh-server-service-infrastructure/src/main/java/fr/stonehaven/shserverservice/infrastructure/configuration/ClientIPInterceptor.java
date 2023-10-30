b89e1383525e5bf8a9767ee33de8a1cda30a9942
Léo Moriceau <thisis41@protonmail.com>
2023-10-30 10:55:08 +0100

    fix: IP metadata change

--- sh-server-service-infrastructure/src/main/java/fr/stonehaven/shserverservice/infrastructure/configuration/ClientIPInterceptor.java
+++ sh-server-service-infrastructure/src/main/java/fr/stonehaven/shserverservice/infrastructure/configuration/ClientIPInterceptor.java
@@ -6,6 +6,8 @@ import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
 @GrpcGlobalServerInterceptor
 public class ClientIPInterceptor implements ServerInterceptor {
     public static final Context.Key<String> CLIENT_IP_KEY = Context.key("client-ip");
+    public static final Metadata.Key<String> FORWARDED_FOR_KEY = Metadata.Key.of("X-FORWARDED-FOR", Metadata.ASCII_STRING_MARSHALLER);
+    public static final Metadata.Key<String> REAL_IP_KEY = Metadata.Key.of("X-REAL-IP", Metadata.ASCII_STRING_MARSHALLER);
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
@@ -17,6 +19,13 @@ public class ClientIPInterceptor implements ServerInterceptor {
         String clientIP = call.getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR).toString();
         clientIP = clientIP.replace("/", "");
         clientIP = clientIP.split(":")[0];
+        System.out.println("Received request from " + clientIP);
+
+        clientIP = headers.get(FORWARDED_FOR_KEY);
+        System.out.println("Received request from " + clientIP);
+
+        clientIP = headers.get(REAL_IP_KEY);
+        System.out.println("Received request from " + clientIP);
 
         // Put the client's IP address in the call context for access in the service implementation
         Context context = Context.current().withValue(CLIENT_IP_KEY, clientIP);
